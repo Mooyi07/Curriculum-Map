6dfa922d5b08cd05ccc6b9bf56498f91b826b738
Mooyi07 <120180277+Mooyi07@users.noreply.github.com>
2023-12-10 19:24:08 +0800

    Added few subjects and Organized the data

--- assets/js/subjects.js
+++ assets/js/subjects.js
@@ -1,30 +1,101 @@
 const Subjects = [
+
+    // FIRST YEAR FIRST TERM
     {   // Advanced Algebra	
-        "subjectCode": "MATH113-V",
-        "prereq": "", 
-        "equiv_subCode": "MATH113-V",
-        "description": "Advanced Algebra",
-        "units": 3
+        subCode: "MATH113-V",
+        equivCode: "MATH113-V",
+        desc: "Advanced Algebra",
+        units: 3
     },
     {   // Computer Engineering as a Discipline	
-        "subjectCode": "CPE112-V",
-        "prereq": "",
-        "equiv_subCode": "CPE112-V",
-        "description": "Computer Engineering as a Discipline",
-        "units": 2
+        subCode: "CPE112-V",
+        equivCode: "CPE112-V",
+        desc: "Computer Engineering as a Discipline",
+        units: 2
     },
     {   // Discrete Mathematics
-        "subjectCode": "ECE113-V",
-        "prereq": "",
-        "equiv_subCode": "ECE113-V",
-        "description": "Discrete Mathematics",
-        "units": 3
+        subCode: "ECE113-V",
+        equivCode: "ECE113-V",
+        desc: "Discrete Mathematics",
+        units: 3
     },
     {   // Engineering Drawing and Electronics Standards
-        "subjectCode": "ECE111-V",
-        "prereq": "",
-        "equiv_subCode": "ECE111-V",
-        "description": "Engineering Drawing and Electronics Standards",
-        "units": 1
+        subCode: "ECE111-V",
+        equivCode: "ECE111-V",
+        desc: "Engineering Drawing and Electronics Standards",
+        units: 1
+    },
+    {   // Engineering Mathematics
+        subCode: "MATH115-V",
+        equivCode: "MATH115-V",
+        desc: "Engineering Mathematics",
+        units: 5
+    },
+    {   // Engineering Measurement: Metrology/Surveying
+        subCode: "EM112ECE-V",
+        equivCode: "EM112ECE-V",
+        desc: "Engineering Measurement: Metrology/Surveying",
+        units: 2
+    },
+    {   // Physical Fitness and Activities
+        subCode: "PE1-V",
+        equivCode: "PE1-V",
+        desc: "Physical Fitness and Activities",
+        units: 2
+    },
+    {   // Programming Logic and Design
+        subCode: "CPE112A-V",
+        equivCode: "CPE112A-V",
+        desc: "Programming Logic and Design",
+        units: 2
+    },
+    {   // Understanding the Self
+        subCode: "GEC1-V",
+        equivCode: "GEC1-V",
+        desc: "Understanding the Self",
+        units: 3
+    },
+    {   // National Service Training Program 1 (NROTC I/CWTS I)
+        subCode: "NSTP1-V",
+        equivCode: "NSTP1-V",
+        desc: "National Service Training Program 1 (NROTC I/CWTS I)",
+        units: 3
+    }
+
+    // FIRST YEAR SECOND TERM
+
+    ,
+    {   // Chemistry for Engineers	
+        subCode: "CHEM124-V",
+        equivCode: "CHEM124-V",
+        desc: "Chemistry for Engineers",
+        units: 4
+    },
+    {   // Computer Aided Drawing (CAD)
+        subCode: "DRAW132-V",
+        preReq: "ECE111-V",
+        equivCode: "DRAW132-V",
+        desc: "Computer Aided Drawing (CAD)",
+        units: 2
+    },
+    {   // Computer Programming
+        subCode: "COMP132-V",
+        preReq: "CPE112A-V",
+        equivCode: "COMP132-V",
+        desc: "Computer Programming",
+        units: 2
+    },
+    {   // Engineering Calculus 1
+        subCode: "MATH124ECE-V",
+        preReq: "MATH113-V,MATH115-V",
+        equivCode: "MATH124ECE-V",
+        desc: "Engineering Calculus 1",
+        units: 4
+    },
+    {   // Mathematics in the Modern World
+        subCode: "GEC4-V",
+        equivCode: "GEC4-V",
+        desc: "Mathematics in the Modern World",
+        units: 3
     }
 ]
